69cc1b753a6d962de7ca686d2ff75f8913539a00
zhangjuana <you@example.com2633971487@qq.com>
2018-07-25 22:00:41 +0800

    two

---
+++ src/DataSever/OrderMainManager.js
@@ -0,0 +1,71 @@
+import {AddOrderMainURL,SearchOrderURL,UpdateOrderMainURL} from './URLConfig';
+class OrderMainManager{
+    async AddOrderMain(AddressID,ProdectsInForm){
+        try {
+            const orderInForm={
+                AddressID,
+                ProdectsInForm
+            }
+            const res=await fetch(AddOrderMainURL,{
+                method:"post",
+                headers:{
+                    'Accpet':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(orderInForm)
+            })
+            const result=await res.json();
+            return result;
+        } catch (error) {
+            return{
+                success:'false',
+                errorMessage:'网络错误'
+            }
+        }
+    }
+    //查询该用户订单信息
+    async SearchOrder(){
+        try {
+            const res=await fetch(SearchOrderURL,{
+                method:"post",
+                headers:{
+                    'Accpet':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify()
+            })
+            const result=await res.json();
+            return result;
+        } catch (error) {
+            return{
+                success:'false',
+                errorMessage:'网络错误'
+            }
+        }
+    }
+    async UpdateOrder(OrderID,IsOK){
+        try {
+            const CollectProduct={
+                OrderID,
+                IsOK
+            }
+            const res=await fetch(UpdateOrderMainURL,{
+                method:"post",
+                headers:{
+                    'Accpet':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(CollectProduct)
+            })
+            const result=await res.json();
+            return result;
+        } catch (error) {
+            return{
+                success:'false',
+                errorMessage:'网络错误'
+            }
+        }
+    }
+
+}
+export default new OrderMainManager();
---
+++ src/DataSever/ProductManager.js
@@ -0,0 +1,64 @@
+import{SearchByIDURL,SearchProductURL,SearchGoodsTypeURL} from './URLConfig';
+class ProductManager{
+    //根据商品id查询详细信息
+    async SearchByID(id){
+        try {
+            const res=await fetch(SearchByIDURL,{
+                method:"post",
+                headers:{
+                    'Accpet':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(id)
+            })
+            const result=await res.json();
+            return result;
+        } catch (error) {
+            return{
+                state:'false',
+                errorMessage:'网络错误'
+            }
+        }
+    }
+    //根据商品类别查询商品
+    async SearchProduct(GoodsTypeID){
+        try {
+            const res=await fetch(SearchProductURL,{
+                method:"post",
+                headers:{
+                    'Accpet':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(GoodsTypeID)
+            })
+            const result=await res.json();
+            return result;
+        } catch (error) {
+            return{
+                state:'false',
+                errorMessage:'网络错误'
+            }
+        }
+    }
+    //查询商品类别
+    async SearchGoods(){
+        try {
+            const res=await fetch(SearchProductURL,{
+                method:"post",
+                headers:{
+                    'Accpet':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify()
+            })
+            const result=await res.json();
+            return result;
+        } catch (error) {
+            return{
+                state:'false',
+                errorMessage:'网络错误'
+            }
+        }
+    }
+}
+export default new ProductManager();
