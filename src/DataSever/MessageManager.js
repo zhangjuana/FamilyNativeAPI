66fd504282162374485b9620d1a973656e03e944
Your Name <you@example.com>
2018-07-26 09:36:01 +0800

    MessageManager

---
+++ src/DataSever/MessageManager.js
@@ -0,0 +1,28 @@
+import{
+    AddCommentURL,
+}from './URLConfig';
+class MessageManager{
+async AddComment(image,OrderMainID,ProductID,CustomerID,CommentContent){
+    try {
+        const token=localStorage.token;
+        const formData=new FormData();
+        formData.append("image",image);
+        formData.append("OrderMainID",OrderMainID);
+        formData.append("ProductID",ProductID);
+        formData.append("CustomerID",CustomerID);
+        formData.append("CommentContent",CommentContent);
+        const res=await fetch(AddCommentURL,{
+            method:'POST',
+            body:formData
+        });
+        const result=await res.json();
+        console.log(result);
+        return result;
+    } catch (error) {
+        return{
+            success:false,
+            errorMessage:'网络错误'
+        }
+    }
+}
+}
